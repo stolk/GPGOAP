b8323824c09368af9d8bc876c9d4b064d3028d90
Bram Stolk <b.stolk@gmail.com>
2016-01-25 18:49:38 -0800

    Fix mswin build.

--- goap.c
+++ goap.c
@@ -8,6 +8,11 @@ http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
 
+#if defined( _MSC_VER )
+#	define _CRT_SECURE_NO_WARNINGS
+#	define snprintf _snprintf
+#endif
+
 #include "goap.h"
 #include "astar.h"
 
